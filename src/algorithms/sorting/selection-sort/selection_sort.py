3816acd1ce0c5bce9545c2bc35d24fb1bad9486d
Vinicius Alves <vinicius_o.a@live.com>
2020-09-16 12:19:02 -0300

    Adição dos algoritmos de sort

---
+++ src/algorithms/sorting/selection-sort/selection_sort.py
@@ -0,0 +1,16 @@
+def selection_sort(lista):
+    n = len(lista)
+
+    for i in range(n):
+        index_min_value = i
+        for j in range(i + 1, n):
+            if lista[index_min_value] > lista[j]:
+                index_min_value = j
+
+        temp = lista[i]
+        lista[i] = lista[index_min_value]
+        lista[index_min_value] = temp
+
+    return lista
+
+selection_sort([12, 31, 5, 3, 0, 43, 99, 78, 32, 9, 7])
