193e12d17ab53df37b9bc4d156205e8b7a8b7c2e
Vinicius Alves <vinicius_o.a@live.com>
2020-10-15 16:44:04 -0300

    Melhoria no algoritmo quick-sort

--- src/algorithms/sorting/quick-sort/quick_sort.py
+++ src/algorithms/sorting/quick-sort/quick_sort.py
@@ -1,25 +1,10 @@
-def partition(vector, start, end):
-    pivot = vector[end]
-    i = start - 1
+def quick_sort(array):
+    if len(array) < 2:
+        return array
 
-    for j in range(start, end):
-        if vector[j] <= pivot:
-            i += 1
-            vector[i], vector[j] = vector[j], vector[i]
-    vector[i + 1], vector[end] = vector[end], vector[i + 1]
-    return i + 1
+    pivot = array[0]
+    lower = [i for i in array[1:] if i <= pivot]
+    upper = [i for i in array[1:] if i > pivot]
+    return quick_sort(lower) + [pivot] + quick_sort(upper)
 
-
-def quick_sort(vector, start, end):
-    if start < end:
-        position = partition(vector, start, end)
-
-        # Left Side
-        quick_sort(vector, start, position - 1)
-
-        # Right Side
-        quick_sort(vector, position + 1, end)
-    return vector
-
-vector = [12, 31, 5, 3, 0, 43, 99, 78, 32, 9, 7]
-quick_sort(vector, 0, len(vector) - 1)
+quick_sort([12, 31, 5, 3, 0, 43, 99, 78, 32, 9, 7])
