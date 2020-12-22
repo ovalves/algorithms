3376739734c8d213c3d857e99c151b3f7541c905
Vinicius Oliveira <vinicius_o.a@live.com>
2020-12-22 14:05:02 -0300

    Create features.php

---
+++ src/machine-learning/recommender/php/features.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Classe de features
+ */
+class Features
+{
+    /**
+     * Escolaridade
+     */
+    const HIGHSCHOOL                = 0.5776576491728649;
+    const SOMECOLLEGE               = 0.5776576491728649;
+    const BACHELORDEGREEMORE        = 0.5826847429340452;
+    const BACHELORDEGREENOGRADUATED = 0.5666234348219533;
+    const GRADUATEDDEGREE           = 0.6232560327746626;
+
+    /**
+     * Etinia
+     */
+    const WHITE    = 0.6262019760882166;
+    const BLACK    = 0.3928774848903957;
+    const HISPANIC = 0.6047959402892891;
+
+    /**
+     * Regiao
+     */
+    const NEW_ENGLAND  = 0.5207378187676579;
+    const MID_ATLANTIC = 0.5081680890358242;
+    const MIDWEST      = 0.6055689603699992;
+    const SOUTH        = 0.6196116507374831;
+    const MOUNTAIN     = 0.636489791900796;
+    const PACIFIC      = 0.5581111981232817;
+
+    /**
+     * Renda
+     */
+    const POOR = 0.4250974866145632;
+    const MID  = 0.6267862552529242;
+    const RICH = 0.6785247813125838;
+
+    /**
+     * Filho
+     */
+    const NOKIDS = 0.36471612937220166;
+    const KIDS   = 0.858910367889827;
+}
