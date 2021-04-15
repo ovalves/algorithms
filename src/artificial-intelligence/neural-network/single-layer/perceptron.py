83dd277f21b6b33275e4fe6afb5cc5606b2dd624
ovalves <vinicius_o.a@live.com>
2021-04-15 20:34:37 -0300

    Adição do código da rede neural - perceptron de uma camada

---
+++ src/artificial-intelligence/neural-network/single-layer/perceptron.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+class Perceptron:
+    def __init__(self):
+        self.input = np.array([[0,0],[0,1], [1,0], [1,1]])
+        self.output = np.array([0, 1, 1, 1])
+        self.weight = np.array([0.0, 0.0])
+        self.learnRate = 0.1
+        self.stopTraining = False
+        self.countErros = 0
+
+    def stepFunction(self, soma):
+        if (soma >= 1):
+            return 1
+        return 0
+
+    def sumOutput(self, layerInput):
+        return self.stepFunction(layerInput.dot(self.weight))
+
+    def printResult(self) :
+        print('Rede neural treinada')
+        print(self.sumOutput(self.input[0]))
+        print(self.sumOutput(self.input[1]))
+        print(self.sumOutput(self.input[2]))
+        print(self.sumOutput(self.input[3]))
+
+    def train(self):
+        while (self.stopTraining == False):
+            self.stopTraining = True
+
+            for i in range(len(self.output)):
+                error = self.output[i] - self.sumOutput(np.asarray(self.input[i]))
+                self.countErros += error
+
+                if (error > 0) :
+                    self.stopTraining = False
+
+                for j in range(len(self.weight)):
+                    self.weight[j] = self.weight[j] + (self.learnRate * self.input[i][j] * error)
+                    print('Peso atualizado: ' + str(self.weight[j]))
+
+            print('Total de erros: ' + str(self.countErros))
+
+        self.printResult()
+
+if __name__ == '__main__':
+    Perceptron().train()
