77e2dac9509d5b409621c9508b2b89d4092ec43f
Vinicius Alves <cit.vinicius@locamerica.tec.br>
2022-08-30 15:16:35 -0300

    ...

--- src/problems/reverse-integer/solution.py
+++ src/problems/reverse-integer/solution.py
@@ -1,10 +1,11 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        if not x < 0:
-            x = int(str(x)[::-1])
-        else:
-            x = int(str(x)[:1] + str(x)[1:][::-1])
+        x = int(str(x)[::-1]) if x >= 0 else int(str(x)[:1] + str(x)[1:][::-1])
+        return 0 if x not in range(-1 << 31, (1 << 31) - 1) else x
 
-        if x not in range((-1 << 31), (1 << 31) - 1):
-            return 0
-        return x
+x = Solution()
+print(x.reverse(123))        # Tem que retornar => 321
+print(x.reverse(-54321))     # Tem que retornar => -12345
+print(x.reverse(120))        # Tem que retornar => 21
+print(x.reverse(0))          # Tem que retornar => 0
+print(x.reverse(1534236469)) # Tem que retornar => 0
