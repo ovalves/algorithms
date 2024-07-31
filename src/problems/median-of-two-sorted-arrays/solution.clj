b869cd27b6f31a0a768af3e8e3b486837fe5896e
Vinicius Oliveira <vinicius_o.a@live.com>
2024-07-31 00:24:09 -0300

    ...

--- src/problems/median-of-two-sorted-arrays/solution.clj
+++ src/problems/median-of-two-sorted-arrays/solution.clj
@@ -1,11 +1,11 @@
 (defn find-median-sorted-arrays
      [nums-1 nums-2]
-     (let [merged (flatten (conj nums-1 nums-2))
+     (let [merged (sort (flatten (conj nums-1 nums-2)))
            len (count merged)
            median (quot len 2)]
           (if (= 0 (mod len 2))
-               (float (/ (+ (nth (sort merged) median) (nth (sort merged) (dec median))) 2))
-               (nth (sort merged) median))))
+               (float (/ (+ (nth merged median) (nth merged (dec median))) 2))
+               (nth merged median))))
 
 (println (find-median-sorted-arrays [1 3] [2])) ; 2 
 (println (find-median-sorted-arrays [1 2] [3 4])) ; 2.5
